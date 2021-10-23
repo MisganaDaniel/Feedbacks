f2dc98d7af9a3bf1ab34de0e5e1f03bd49cf838d
MisganaDaniel <danielmisgun@gmail.com>
2021-10-23 21:28:49 +0300

    img

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <div class="card text-center shadow-lg" style="width: 1000px;margin-left:150px;margin-top:100px;border-radius:12px;">
         <div class="card-header">
             <div class="row">
-                <div class="col">
+                <div class="col" style="text-align: left;">
                     <img src="logo.png">
                 </div>
                 <div class="col">
