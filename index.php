6edde11fa2165e749528906fb0df7028bbceb9bd
MisganaDaniel <danielmisgun@gmail.com>
2021-10-23 17:30:32 +0300

    code

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Feedbacks</title>
+    <script src="main.js"></script>
 </head>
 <body>
-    
+    First Name: <input type="text" id="firstName"> 
+    Last Name: <input type="text" id="LasName"> 
+    Email Name: <input type="text" id="Email"> 
+    Feedback: <input type="text" id="Feedback"> 
+    <input type="button" value="Submit" onclick="feedback()">
 </body>
 </html>
